0e62911159d06eb46401d2e3d25a7e37c641a4b4
daniel melenge <danielmelenge@gmail.com>
2023-04-04 09:51:16 -0500

    Initial Commit

---
+++ selected/selectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  selectedApp.swift
+//  selected
+//
+//  Created by daniel melenge rojas on 4/04/23.
+//
+
+import SwiftUI
+
+@main
+struct selectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ selectedUITests/selectedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  selectedUITestsLaunchTests.swift
+//  selectedUITests
+//
+//  Created by daniel melenge rojas on 4/04/23.
+//
+
+import XCTest
+
+final class selectedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
