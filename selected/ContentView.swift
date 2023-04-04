cf441ae6f15ecc68b4b88c25d9855b562331cb64
daniel melenge <danielmelenge@gmail.com>
2023-04-04 10:17:39 -0500

    Change in the view of ContentView

--- selected/ContentView.swift
+++ selected/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var colors = ["Red",    "Green",    "Blue", "Yellow",   "Starlight",    "Purple",   "Deep Purple"]
+    @State private var  selectedColor   =   "Red"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Picker("Please choose a color", selection: $selectedColor)  {
+                ForEach (colors, id: \.self) {
+                    Text($0)
+                }
+                        
+                }
+                        Text("You selected: \(selectedColor)")
+                    }
+                        
+                }
+                        
+            }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
